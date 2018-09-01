0ae2df377b22877c7af295f62939fa25f6c7f23e
shreyashpandey <34275495+shreyashpandey@users.noreply.github.com>
2018-09-02 00:34:45 +0530

    Add files via upload

---
+++ index2.php
@@ -0,0 +1,104 @@
+<?php
+  // Create database connection
+  $db = mysqli_connect("localhost", "root", "Uke2LEAeTG0Oo5F4", "mydb1");
+
+  // Initialize message variable
+  $msg = "";
+
+  // If upload button is clicked ...
+  if (isset($_POST['upload'])) {
+  	// Get image name
+  	$image = $_FILES['image']['name'];
+  	// Get text
+  	$image_text = mysqli_real_escape_string($db, $_POST['image_text']);
+
+  	// image file directory
+  	$target = "images/".basename($image);
+
+  	$sql = "INSERT INTO images (image, image_text) VALUES ('$image', '$image_text')";
+  	// execute query
+  	mysqli_query($db, $sql);
+
+  	if (move_uploaded_file($_FILES['image']['tmp_name'], $target)) {
+  		$msg = "Image uploaded successfully";
+  	}else{
+  		$msg = "Failed to upload image";
+  	}
+  }
+  $result = mysqli_query($db, "SELECT * FROM images Order by id desc") or die("Error: " . mysqli_error($db));
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Image Upload</title>
+<style type="text/css">
+   #content{
+   	width: 50%;
+   	margin: 20px auto;
+   	border: 1px solid #cbcbcb;
+   }
+   form{
+   	width: 50%;
+   	margin: 20px auto;
+   }
+   form div{
+   	margin-top: 5px;
+   }
+   #img_div{
+   	width: 80%;
+   	padding: 5px;
+   	margin: 15px auto;
+   	border: 1px solid #cbcbcb;
+   }
+   #img_div:after{
+   	content: "";
+   	display: block;
+   	clear: both;
+   }
+   img{
+   	float: left;
+   	margin: 5px;
+   	width: 300px;
+   	height: 140px;
+   }
+</style>
+</head>
+<body>
+<div id="content">
+  <?php
+  $c=0;
+    //while () {
+
+      $row = mysqli_fetch_array($result);
+      //$last_key = key( array_slice( $row, -1, 1, TRUE ) );
+      //$c= end($row);
+      //$reversed = array_reverse($row);
+      //$l=mysqli_insert_id($db);
+      echo "<div id='img_div'>";
+      	echo "<img src='images/".$row['image']."' >";
+      	echo "<p>".$row['image_text']."</p>";
+      //echo $row['image'] ;end($row)
+      echo "</div>";
+      //c++;
+    //}
+  ?>
+  <form method="POST" action="index2.php" enctype="multipart/form-data">
+  	<input type="hidden" name="size" value="1000000">
+  	<div>
+  	  <input type="file" name="image">
+  	</div>
+  	<div>
+      <textarea 
+      	id="text" 
+      	cols="40" 
+      	rows="4" 
+      	name="image_text" 
+      	placeholder="Say something about this image..."></textarea>
+  	</div>
+  	<div>
+  		<button type="submit" name="upload">POST</button>
+  	</div>
+  </form>
+</div>
+</body>
+</html>
